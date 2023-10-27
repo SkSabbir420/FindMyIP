aca297ed8f5acda42ea52e27321ec2c3167207a1
SkSabbir420 <sksabbirhosen420@gmail.com>
2023-10-27 11:35:54 +0600

    Basic version complete

--- app/src/main/java/com/example/android/findmyip/MainActivity.kt
+++ app/src/main/java/com/example/android/findmyip/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android.findmyip.ui.theme.FindMyIPTheme
 import com.example.android.findmyiplibrary.FindMyIP
 import kotlinx.coroutines.GlobalScope
@@ -29,12 +33,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         var information by mutableStateOf<String>("")
+        var progressStatus by mutableStateOf(true)
 
         GlobalScope.launch {
             val myIp = FindMyIP.getPublicIP()
-            Log.d("myIpp",myIp)
+//            Log.d("myIpp",myIp)
             information = FindMyIP.execute().getIPInformation(myIp).body().toString()
-            Log.d("IpInformaion",information)
+            progressStatus = false
+//            Log.d("IpInformaion",information)
         }
 
         setContent {
@@ -44,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(information)
+                    FindMyIPScreen(information,progressStatus)
                 }
             }
         }
@@ -52,13 +58,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(information:String) {
+fun FindMyIPScreen(information:String,progressStatus:Boolean) {
     Column(modifier = Modifier
         .fillMaxSize()
-        .fillMaxWidth(),
+        .fillMaxWidth()
+        .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        if (progressStatus)
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.surfaceVariant
+        )
         Text(text = "$information")
     }
 
